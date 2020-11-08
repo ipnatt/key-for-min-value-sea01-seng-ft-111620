8f0b907c223c06d074ac2f14c41f11ce42a3ac02
Irvin Natt <ipnatt@gmail.com>
2020-11-08 22:27:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,3 @@
 def key_for_min_value(name_hash)
   minimum_values = nil
   minimum_keys = nil
-  name_has.each do |legend, price|
-    if minimum_values == nil
-      minimum_values = price
-      minimum_keys = legend
-    elsif minimum_values > price
-      minimum_values = values
-      minimum_keys = legend
-    end
-  end
-  minimum_keys
-end
